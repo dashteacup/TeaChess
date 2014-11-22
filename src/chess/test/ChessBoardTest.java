fd00fa690a287761a6514780181ddded729f055b
Paul Curry <dashteacup@gmail.com>
2014-11-22 15:21:57 -0600

    Expand ChessBoard init tests.
    I broke apart some of the initializeFullBoardAndCheckBounds tests. I
    also added comments to every test.

--- src/chess/test/ChessBoardTest.java
+++ src/chess/test/ChessBoardTest.java
@@ -20,20 +20,43 @@ import chess.Rook;
  * TODO: I need to actually finish writing tests here.
  */
 public class ChessBoardTest {
+    /**
+     * Confirm that the ChessBoard has the proper size and shape.
+     */
     @Test
     public void initializeFullBoardAndCheckBounds()
     {
         ChessBoard b = new ChessBoard();
+        // number of rows
         assertEquals(9, b.getBoard().size());
+        // number of columns
         assertEquals(9, b.getBoard().get(1).size());
+        assertEquals(9, b.getBoard().get(8).size());
+        // check zero row, zero row has no columns
+        assertEquals(0, b.getBoard().get(0).size());
+    }
 
+    /**
+     * Ensure elements that should be null are null.
+     */
+    @Test
+    public void initializeAndCheckForNull()
+    {
+        ChessBoard b = new ChessBoard();
+        // check zero column
+        assertNull(b.getBoard().get(1).get(0));
+        assertNull(b.getBoard().get(8).get(0));
         // check middle
+        assertNull(b.getBoard().get(3).get(2));
         assertNull(b.getBoard().get(4).get(4));
         assertNull(b.getBoard().get(5).get(1));
         assertNull(b.getBoard().get(5).get(8));
         assertNull(b.getBoard().get(6).get(7));
     }
 
+    /**
+     * Confirm that all the white pieces are in their proper starting position.
+     */
     @Test
     public void intitializeFullBoardAndCheckWhiteSide()
     {
@@ -57,6 +80,9 @@ public class ChessBoardTest {
         assertEquals(ChessPieceColor.WHITE, b.getBoard().get(2).get(8).getColor());
     }
 
+    /**
+     * Confirm that all the black pieces are in their proper starting position.
+     */
     @Test
     public void initializeFullBoardAndCheckBlackSide()
     {
@@ -71,7 +97,7 @@ public class ChessBoardTest {
         assertTrue(b.getBoard().get(8).get(6) instanceof Bishop);
         assertTrue(b.getBoard().get(8).get(7) instanceof Knight);
         assertTrue(b.getBoard().get(8).get(8) instanceof Rook);
-        assertTrue(b.getBoard().get(7).get(8) instanceof Pawn);
+        assertTrue(b.getBoard().get(7).get(1) instanceof Pawn);
         assertTrue(b.getBoard().get(7).get(5) instanceof Pawn);
         assertTrue(b.getBoard().get(7).get(8) instanceof Pawn);
 
